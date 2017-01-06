eb295f92d255eb9f5ce8be845e5c03f6c8080024
Wang Han <416810799@qq.com>
2017-01-06 23:16:16 +0800

    SelfHook: Adjust Application Config
    The app name can be different, so it is a bad practice.
    
    Use build config to handle it.

--- app/src/main/java/com/aviraxp/adblocker/continued/hook/SelfHook.java
+++ app/src/main/java/com/aviraxp/adblocker/continued/hook/SelfHook.java
@@ -1,13 +1,15 @@
 package com.aviraxp.adblocker.continued.hook;
 
+import com.aviraxp.adblocker.continued.BuildConfig;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class SelfHook implements IXposedHookLoadPackage {
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals("com.aviraxp.adblocker.continued")) {
-            Class<?> SettingsActivity = XposedHelpers.findClass("com.aviraxp.adblocker.continued.ui.SettingsActivity", lpparam.classLoader);
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (lpparam.packageName.equals(BuildConfig.APPLICATION_ID)) {
+            Class<?> SettingsActivity = XposedHelpers.findClass(BuildConfig.APPLICATION_ID + ".ui.SettingsActivity", lpparam.classLoader);
             XposedHelpers.setStaticBooleanField(SettingsActivity, "isActivated", true);
         }
     }
