c3026f2f0db5051042306f232a95fe80c93dac07
Wang Han <416810799@qq.com>
2017-02-23 22:21:00 +0800

    All: Flag to Disable Hook on System Applications
    Someone requests this.
    
    So do this.

--- app/src/main/java/com/aviraxp/adblocker/continued/hook/HookLoader.java
+++ app/src/main/java/com/aviraxp/adblocker/continued/hook/HookLoader.java
@@ -1,11 +1,16 @@
 package com.aviraxp.adblocker.continued.hook;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
+import com.aviraxp.adblocker.continued.helper.PreferencesHelper;
 import com.aviraxp.wechatdonationhelper.Donation;
 
 import java.util.HashSet;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class HookLoader implements IXposedHookLoadPackage, IXposedHookZygoteInit {
@@ -20,13 +25,26 @@ public class HookLoader implements IXposedHookLoadPackage, IXposedHookZygoteInit
     static HashSet<String> urlList;
 
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+        new ServicesHook().hook(lpparam);
+
+        if (lpparam.packageName.equals("android")) {
+            return;
+        }
+
+        Object activityThread = XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", null), "currentActivityThread");
+        Context systemContext = (Context) XposedHelpers.callMethod(activityThread, "getSystemContext");
+        ApplicationInfo info = systemContext.getPackageManager().getApplicationInfo(lpparam.packageName, 0);
+        if ((info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0 && PreferencesHelper.isDisableSystemApps()) {
+            return;
+        }
+
         new ActViewHook().hook(lpparam);
         new BackPressHook().hook(lpparam);
         new HidingHook().hook(lpparam);
         new HostsHook().hook(lpparam);
         new ReceiversHook().hook(lpparam);
         new SelfHook().hook(lpparam);
-        new ServicesHook().hook(lpparam);
         new WebViewHook().hook(lpparam);
         Donation.hook(lpparam, "wxid_90m10eigpruz21");
     }
