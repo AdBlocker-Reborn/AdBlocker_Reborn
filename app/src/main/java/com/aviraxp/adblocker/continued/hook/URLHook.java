aca8223fb6760ad9e41afeadc329b0c3c063827a
Wang Han <416810799@qq.com>
2017-03-14 14:38:32 +0800

    URLHook: Redirect to Local Proxy instead of Skipping Method

--- app/src/main/java/com/aviraxp/adblocker/continued/hook/URLHook.java
+++ app/src/main/java/com/aviraxp/adblocker/continued/hook/URLHook.java
@@ -1,19 +1,19 @@
 package com.aviraxp.adblocker.continued.hook;
 
 import com.aviraxp.adblocker.continued.helper.PreferencesHelper;
+import com.aviraxp.adblocker.continued.util.DecodeUtils;
 import com.aviraxp.adblocker.continued.util.LogUtils;
 
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 class URLHook {
-
-    private static final String BLOCK_MESSAGE = "Blocked by AdBlocker Reborn: ";
-    private static String url = null;
 
     public void hook(final XC_LoadPackage.LoadPackageParam lpparam) {
 
@@ -24,6 +24,7 @@ class URLHook {
         XC_MethodHook urlHook = new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) {
+                String url;
                 if (param.args.length == 1) {
                     url = (String) param.args[0];
                 } else {
@@ -33,16 +34,22 @@ class URLHook {
                     String urlCutting = url.substring(url.indexOf("://") + 3);
                     for (String host : HookLoader.hostsList) {
                         if (urlCutting.startsWith(host)) {
-                            param.setResult(null);
-                            param.setThrowable(new MalformedURLException(BLOCK_MESSAGE + url));
+                            if (param.args.length == 1) {
+                                param.args[0] = "http://localhost";
+                            } else {
+                                param.args[1] = "http://localhost";
+                            }
                             LogUtils.logRecord("URL Block Success: " + lpparam.packageName + "/" + url);
                             return;
                         }
                     }
                     for (String adUrl : HookLoader.urlList) {
                         if (urlCutting.contains(adUrl)) {
-                            param.setResult(null);
-                            param.setThrowable(new MalformedURLException(BLOCK_MESSAGE + url));
+                            if (param.args.length == 1) {
+                                param.args[0] = "http://localhost";
+                            } else {
+                                param.args[1] = "http://localhost";
+                            }
                             LogUtils.logRecord("URL Block Success: " + lpparam.packageName + "/" + url);
                             return;
                         }
@@ -56,16 +63,40 @@ class URLHook {
             protected void beforeHookedMethod(MethodHookParam param) {
                 String host = (String) param.args[1];
                 if (host != null && !PreferencesHelper.whiteListElements().contains(host) && HookLoader.hostsList.contains(host)) {
-                    param.setResult(null);
-                    param.setThrowable(new MalformedURLException(BLOCK_MESSAGE + host));
+                    param.args[1] = "localhost";
                     LogUtils.logRecord("URL Block Success: " + lpparam.packageName + "/" + host);
+                }
+            }
+        };
+
+        XC_MethodHook urlConnectionHook = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws MalformedURLException {
+                URL url = (URL) param.args[0];
+                if (url != null) {
+                    String host = url.getHost();
+                    if (host != null && !PreferencesHelper.whiteListElements().contains(host) && HookLoader.hostsList.contains(host)) {
+                        param.args[0] = new URL("http://localhost");
+                        return;
+                    }
+                    String urlDecode = DecodeUtils.decode(url.toString(), null);
+                    if (urlDecode != null) {
+                        for (String adUrl : HookLoader.urlList) {
+                            if (urlDecode.contains(adUrl)) {
+                                param.args[0] = new URL("http://localhost");
+                                return;
+                            }
+                        }
+                    }
                 }
             }
         };
 
         XposedHelpers.findAndHookConstructor(URL.class, String.class, urlHook);
         XposedHelpers.findAndHookConstructor(URL.class, URL.class, String.class, urlHook);
-        XposedHelpers.findAndHookConstructor(URL.class, String.class, String.class, int.class, String.class, hostsHook);
         XposedHelpers.findAndHookConstructor(URL.class, String.class, String.class, String.class, hostsHook);
+        XposedHelpers.findAndHookConstructor(URL.class, String.class, String.class, int.class, String.class, hostsHook);
+        XposedHelpers.findAndHookConstructor(URLConnection.class, URL.class, urlConnectionHook);
+        XposedHelpers.findAndHookConstructor(HttpURLConnection.class, URL.class, urlConnectionHook);
     }
 }
