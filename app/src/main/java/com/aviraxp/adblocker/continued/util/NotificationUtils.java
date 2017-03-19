a9cddd783904bf8a8fcfac393bf395a0e66641dd
Wang Han <416810799@qq.com>
2017-03-19 22:54:12 +0800

    NotificationUtils: Introduce Notification Show

---
+++ app/src/main/java/com/aviraxp/adblocker/continued/util/NotificationUtils.java
@@ -0,0 +1,51 @@
+package com.aviraxp.adblocker.continued.util;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.aviraxp.adblocker.continued.R;
+import com.aviraxp.adblocker.continued.ui.SettingsActivity;
+
+public class NotificationUtils extends BroadcastReceiver {
+
+    public static void setNotify(String title, String description, String thirdline, int id, Context ctx) {
+        Intent postNotification = new Intent("AdBlocker.intent.action.POST_NOTIFICATION");
+        postNotification.putExtra("description", description)
+                .putExtra("thirdline", thirdline)
+                .putExtra("id", id)
+                .putExtra("title", title);
+        ctx.sendBroadcast(postNotification);
+    }
+
+    private void postNotification(String title, String description, int id, Context ctx) {
+        PendingIntent pi = PendingIntent.getActivity(ctx, 0, new Intent(ctx, SettingsActivity.class), 0);
+        Notification.Builder notification = new Notification.Builder(ctx)
+                .setTicker(description)
+                .setContentTitle(title)
+                .setContentText(description)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(pi)
+                .setAutoCancel(true);
+        NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(id, notification.getNotification());
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Bundle extras = intent.getExtras();
+        boolean hasExtras = extras != null;
+        if ("AdBlocker.intent.action.POST_NOTIFICATION".equals(action)) {
+            if (hasExtras) {
+                String description = extras.getString("description");
+                int id = extras.getInt("id");
+                String title = extras.getString("title");
+                postNotification(title, description,  id, context);
+            }
+        }
+    }
+}
