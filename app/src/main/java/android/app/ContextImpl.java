0143e6ee788fb793103fe74de8ef5b5a67900b27
Wang Han <416810799@qq.com>
2017-04-20 00:11:26 +0800

    HiddenAPI: Throw Exception when Using Built-in Constructor

--- app/src/main/java/android/app/ContextImpl.java
+++ app/src/main/java/android/app/ContextImpl.java
@@ -3,4 +3,7 @@ package android.app;
 import android.content.Context;
 
 public abstract class ContextImpl extends Context {
+    public ContextImpl() {
+        throw new UnsupportedOperationException();
+    }
 }
