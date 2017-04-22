859dda28b5276f83d5d0d2aea26ba71339d5c000
Wang Han <416810799@qq.com>
2017-04-22 19:01:02 +0800

    Revert "HiddenAPI: Throw Exception when Using Built-in Constructor"
    This reverts commit 0143e6ee788fb793103fe74de8ef5b5a67900b27.

--- app/src/main/java/android/app/ContextImpl.java
+++ app/src/main/java/android/app/ContextImpl.java
@@ -3,7 +3,4 @@ package android.app;
 import android.content.Context;
 
 public abstract class ContextImpl extends Context {
-    public ContextImpl() {
-        throw new UnsupportedOperationException();
-    }
 }
